2d226c8efdf6a62ab8dec1c557f1a75a73ef8a10
dima-yurchuk <dimayurchuk20@gmail.com>
2021-02-10 17:25:57 +0200

    Task 1.3

--- lab1/tcp_client.py
+++ lab1/tcp_client.py
@@ -6,9 +6,9 @@ host = 'localhost'
 port = 5555
 
 socket_client.connect((host, port)) # встановлення з'єднання
-
-message = input("Enter message from client:\n")
-socket_client.send(message.encode('utf-8'))
-server_message = socket_client.recv(1024)
-print('Message from server: ' + server_message.decode('utf-8'))
-socket_client.close()
+while True:
+    message = input("Enter client message:\n")
+    socket_client.send(message.encode('utf-8'))
+    if message == 'stop':
+        socket_client.close()
+        break
--- lab1/tcp_server.py
+++ lab1/tcp_server.py
@@ -1,7 +1,6 @@
 import socket
 from datetime import datetime
 import time
-import sys
 
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,14 +10,10 @@ port = 5555 #номер порта
 server_socket.bind((host, port)) # зв'язування ip адреси з номером порта
 server_socket.listen(5)
 print('The server is waiting for connection......')
-
 client_socket, addr = server_socket.accept()
-client_message = client_socket.recv(1024).decode('utf-8')
-print('Client message: ' + client_message +
-      "\nTime to receive the message: " + str(datetime.now()))
-time.sleep(5)
-size_resv_bytes = client_socket.send(client_message.encode('utf-8'))
-if size_resv_bytes == len(client_message): # перевірка, чи всі дані були надіслані
-    print("All data sent successfully")
-else:
-    print("!!!Error when sending data!!!")
+while True:
+    client_message = client_socket.recv(1024)
+    if client_message.decode('utf-8') == 'stop':
+        server_socket.close()
+        break
+    print('Client message:' + client_message.decode('utf-8'))
