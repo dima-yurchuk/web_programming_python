287759e78b1c7e3d4515f50488a4ca9564085465
dima-yurchuk <dimayurchuk20@gmail.com>
2021-02-11 10:33:57 +0200

    Task 1.5 non blocking

--- lab1/tcp_client.py
+++ lab1/tcp_client.py
@@ -6,9 +6,9 @@ host = 'localhost'
 port = 5555
 
 socket_client.connect((host, port)) # встановлення з'єднання
-socket_client.setblocking(True)
+socket_client.setblocking(False)
 message = 'Test message!' * 10000000
 a = socket_client.send(message.encode('utf-8'))
-print('Count recieved data - blocking - mode:' + str(a))
+print('Count recieved data - non blocking - mode:' + str(a))
 socket_client.send('stop'.encode('utf-8'))
 socket_client.close()
