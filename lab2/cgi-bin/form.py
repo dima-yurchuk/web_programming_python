a0ab6eca48659c3d5ccf23ec70ca321262bd6709
dima-yurchuk <dimayurchuk20@gmail.com>
2021-02-14 11:45:28 +0200

    Lab2

---
+++ lab2/cgi-bin/form.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import cgi
+import sys
+import codecs
+import os
+import http.cookies
+
+cookie = http.cookies.SimpleCookie(os.environ.get("HTTP_COOKIE"))
+cookie_value = cookie.get("count_f")
+if cookie_value is None:
+    print(f"Set-cookie: count_f={1}")
+    count_forms = 1
+else:
+    count_forms = int(cookie_value.value) + 1
+    print(f"Set-cookie: count_f={count_forms}")
+
+print("Content-type: text/html\r\n")
+print() # This is mandatory !!!
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+form = cgi.FieldStorage()
+
+
+lastname = form.getfirst("lastname", "не вказано")
+firstname = form.getfirst("firstname", "не вказано")
+if form.getvalue('expirience'):
+   experience = form.getvalue('expirience')
+else:
+   experience = "не вибрано"
+programming_language = form.getlist('prog_language')
+print("""<!DOCTYPE HTML>
+ <html>
+ <head>
+ <meta charset="utf-8">
+ <title>Form Processing</title>
+ </head>
+ <body style='background-color:#CADABA;'>""")
+print(f"""<div style='font-size:25px;position:relative; left:38%;'><b>Прізвище:</b>{firstname}<br>
+<b>Ім'я:</b>{lastname}<br>
+<b>Мови програмування:</b>""")
+if len(programming_language) == 0:
+    text = 'не вказано'
+else:
+    text = ''
+    for elem in programming_language:
+        text += elem+', '
+    text = text[0:-2]
+print(f"""{text}<br>
+<b>Досвід роботи:</b>{experience}<br>
+<b>Кількість заповнених форм:</b>{count_forms}</div><br>
+""")
+print("<div style='position:relative; left:45%;'><a href='../index.html' style='font-size:30px'>Головна</a></div>")
+print("""</body>
+ </html>""")
